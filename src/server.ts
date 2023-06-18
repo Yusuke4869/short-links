d7f6d9b08b8e0d0bc85157a69cb9c120b46cd103
yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2023-06-18 19:15:13 +0900

    ✨ add host address

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,12 @@ import app from "./app";
 
 dotenv.config();
 
+const HOST = process.env.HOST ?? "127.0.0.1";
 const PORT = Number(process.env.PORT) ?? 3000;
 
 const start = async () => {
   try {
-    await app.listen({ port: PORT });
+    await app.listen({ host: HOST, port: PORT });
   } catch (error) {
     console.error(error);
     process.exit(1);
