0c2602bc07c781f6974461862a217677c178a110
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-01-05 21:59:25 +0900

    🔨 update restart script

--- restart.sh
+++ restart.sh
@@ -1,10 +1,13 @@
+PATH=$PATH:$HOME/.volta/bin
+
 git checkout .
 git pull origin main
+
 yarn install
-yarn build
+yarn run build
 
 for PID in $(ps -e -o pid,cmd | grep yarn | grep start-short-links | awk '{print $1}'); do
   kill $PID
 done
 
-nohup yarn start-short-links &
+nohup yarn run start-short-links &
